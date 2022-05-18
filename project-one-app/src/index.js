0d4d34f50d516fd6465fe96a97aed224c13b1486
Geofrey Bisonga <bisongageofrey@gmail.com>
2022-05-18 11:16:26 +0300

    commits

---
+++ project-one-app/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM  from 'react'
+import Navbar from './Navbar';
+
+function Index() {
+  return (
+    <Navbar />
+  ) 
+}
+
+ReactDOM.render(<Index />, document.getElementById('root'));
+
+
